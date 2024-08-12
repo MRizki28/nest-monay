a728e1cd687da96f16c2cb73f75a51c774b8940c
MRizki28 <muhammadrizkitkj123@gmail.com>
2024-08-12 20:05:10 +0800

    feat(account):create account migration

---
+++ src/models/user.model.ts
@@ -0,0 +1,39 @@
+import { Column, CreatedAt, DataType, Model, Table, UpdatedAt } from "sequelize-typescript";
+
+@Table({
+    tableName: 'tb_users'
+})
+
+export class UserModel extends Model<UserModel>{
+    @Column({
+        type: DataType.UUID,
+        defaultValue: DataType.UUIDV4,
+        allowNull: false,
+        primaryKey: true
+    })
+    id: string; 
+
+    @Column({
+        type: DataType.STRING,
+        allowNull: false
+    })
+    name: string;
+
+    @Column({
+        type: DataType.STRING,
+        allowNull: false
+    })
+    email: string;
+
+    @Column({
+        type: DataType.STRING,
+        allowNull: false
+    })
+    password: string;
+
+    @CreatedAt
+    created_at: Date;
+
+    @UpdatedAt
+    updated_at: Date;
+}
