7963d5841df4ee309f80b57305871b29808522e4
MRizki28 <muhammadrizkitkj123@gmail.com>
2024-08-14 21:46:33 +0800

    add authentication

--- src/service/account/account.service.ts
+++ src/service/account/account.service.ts
@@ -1,18 +1,18 @@
 import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/sequelize';
 import { stat } from 'fs';
-import { MonayModel } from 'src/models/account.model';
+import { AccountModel } from 'src/models/account.model';
 
 @Injectable()
 export class AccountService {
     constructor(
-        @InjectModel(MonayModel)
-        private readonly monayModel: typeof MonayModel,
+        @InjectModel(AccountModel)
+        private readonly accountModel: typeof AccountModel,
     ) {}
 
     async getAllData(): Promise<any> {
         try {
-            const data = await this.monayModel.findAll();
+            const data = await this.accountModel.findAll();
             if(data.length == 0) {
                 return [{
                     message : "Data Found",
