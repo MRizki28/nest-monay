2d5a716bddba1847fb5e3a9117a1779be4d1049b
MRizki28 <muhammadrizkitkj123@gmail.com>
2024-08-14 21:45:29 +0800

    add authentication

--- src/database/migrations/20240812034402-tb_account.js
+++ src/database/migrations/20240812034402-tb_account.js
@@ -3,7 +3,7 @@
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.createTable('tb_monay', {
+    await queryInterface.createTable('tb_account', {
       id: {
         type: Sequelize.UUID,
         defaultValue: Sequelize.UUIDV4,
@@ -44,6 +44,6 @@ module.exports = {
   },
 
   async down(queryInterface, Sequelize) {
-    await queryInterface.dropTable('tb_monay')
+    await queryInterface.dropTable('tb_account')
   }
 };
